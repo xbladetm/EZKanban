3f0d3bde051ef98b00a50e80a1895e4c08c79435
Andrei Mirza <intelizedbik3r@gmail.com>
2013-05-14 04:14:45 +0200

    Added lib folder with MySQL library used to connect to the MySQL database.

---
+++ EZKanban/src/ezkanban/DatabaseConnection/DBConnect.java
@@ -0,0 +1,17 @@
+package ezkanban.DatabaseConnection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+@SuppressWarnings("unused")
+public class DBConnect {
+	private Connection connect = null;
+	  private Statement statement = null;
+	  private PreparedStatement preparedStatement = null;
+	  private ResultSet resultSet = null;
+
+	  // TODO finish this class
+}
