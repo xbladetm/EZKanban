4fb4a6fd552bdde8dc92ca401cec65880629e5ad
Andrei Mirza <intelizedbik3r@gmail.com>
2013-05-21 02:51:49 +0200

    started task

--- EZKanban/src/ezkanban/common/Task.java
+++ EZKanban/src/ezkanban/common/Task.java
@@ -1,5 +1,29 @@
 package ezkanban.common;
 
+import java.sql.Date;
+
 public class Task {
+	
+	private enum Priority {
+		VERYHIGH, HIGH, MEDIUM, LOW
+	}
+	
+	private enum Status {
+		NOTSTARTED, INPROGRESS, DONE, CANCELLED
+	}
+	
+	private int taskID;
+	private String description;	
+	private Priority priority;
+	private int userID;
+	private Date added;
+	private Date completed;	
+	private Status status;
+	private int scrumUnits;
+	
+	public Task (String allParams){
+		//Tokenizer goes here; 
+	}
+	
 
 }
